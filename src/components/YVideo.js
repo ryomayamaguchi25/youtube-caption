d0a0401d3fb6a6c8494462077b4b4139d0cd20aa
RY <kurodaizu0810@gmail.com>
2020-05-09 20:57:38 +0900

    make video wrap

--- src/components/YVideo.js
+++ src/components/YVideo.js
@@ -1,31 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-
-
-// IFrame Player API の読み込み
-// var tag = document.createElement('script');
-// tag.src = "https://www.youtube.com/iframe_api";
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-function onYouTubeIframeAPIReady() {
-  // ytPlayer = new YT.Player(
-  //   'sample', // 埋め込む場所の指定
-  //   {
-  //     width: 640, // プレーヤーの幅
-  //     height: 390, // プレーヤーの高さ
-  //     videoId: 'bHQqvYy5KYo' // YouTubeのID
-  //   }
-  // );
-  console.log('load');
-};
-// YouTubeの埋め込み
-
+import YouTube from 'react-youtube';
 
 export default function YVideo() {
+
+  const [time, setTime] = useState(0);
+
+  var timer;
+  const id = '8rry2jde8j0';
+  function getTime(event) {
+    timer = setInterval(function () {
+      var time = event.target.getCurrentTime();
+      var time = Math.floor(time * 100) / 100;
+      setTime(time)
+    }, 100);
+  }
+  function stopTimer() {
+    clearInterval(timer);
+  }
   return (
-    <div id='sample'>
-      <script src="https://www.youtube.com/player_api"></script>
-    </div>
+    <React.Fragment>
+      <YouTube id='video' videoId={id} onPlay={getTime} onPause={stopTimer} ></YouTube>
+      <div>{time}</div>
+    </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import YT from './components/YT';
+import YTWrap from './components/YTWrap';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <YT />
+    <YTWrap />
   </React.StrictMode>,
   document.getElementById('root')
 );
